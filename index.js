54ceb2de657b10e2f94a4dfb1f70226f5837b18c
zishanmeh <zishanmehedihasan2@gmail.com>
2024-07-22 00:55:01 +0100

    Added brands dynamically

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ menuBar.addEventListener('click', () => {
         navItems.classList.add("hide")
     }
 })
+
+// Added brands image in brand section
+
+const brandSection = document.getElementById("brands")
+fetch('data/brands.json')
+    .then((response) => response.json())
+    .then((brands) => {
+        
+        for(let i = 0; i < brands.length; i++){
+            const newBrand = document.createElement('img');
+            newBrand.src = brands[i].url;
+
+            brandSection.appendChild(newBrand);
+        }
+
+    } );
